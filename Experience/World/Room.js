f22103caeb48dd00569872c7617524ec290c9256
= <ales.volivares@gmail.com>
2023-03-28 23:20:12 +0200

    Avances hasta importación

---
+++ Experience/World/Room.js
@@ -0,0 +1,9 @@
+import * as THREE from 'three';
+import Experience from '../Experience';
+
+export default class Room {
+    constructor() {
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+    }
+}
